7bbde24441954e23f1f53220f521eb9d5b20efdd
youliangdao <youliangdaojing5@gmail.com>
2023-05-07 14:26:17 +0900

    Add head metadata to the first post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import Link from "next/link";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <Link href="/">Back to home</Link>
     </>
