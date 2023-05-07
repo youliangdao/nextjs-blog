9e749459787e631b16e9654ee85637926129a49c
youliangdao <youliangdaojing5@gmail.com>
2023-05-07 14:43:45 +0900

    Add Layout component and CSS module

--- src/pages/posts/first-post.js
+++ src/pages/posts/first-post.js
@@ -1,15 +1,15 @@
 import Head from "next/head";
 import Link from "next/link";
-import Script from "next/script";
+import Layout from "../../components/Layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
       <Link href="/">Back to home</Link>
-    </>
+    </Layout>
   );
 }
