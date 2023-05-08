238da6cefd119f08189b49779f2310cdc56ea0b5
youliangdao <youliangdaojing5@gmail.com>
2023-05-08 20:07:33 +0900

    Polish the Index Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 // Add this import at the top of the file
 import { getSortedPostsData } from "../../lib/posts";
+import Link from "next/link";
+import { Date } from "../components/date";
 
 export default function Home({ allPostsData }) {
   return (
@@ -25,11 +27,12 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                {/* Add this <small> tag below the existing <small> tag */}
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
